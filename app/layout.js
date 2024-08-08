5a8aa7bb4d44b42ea3ec91caa7fd3f2baf0d8a8b
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-08-08 16:56:40 +0200

    Tested new currencies.

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,21 @@ import "./globals.css";
 export const metadata = {
   title: "K Trader",
   description: packageJsonFile.description,
+  manifest: "/manifest.json",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+  ],
+  icons: {
+    shortcut: { type: "image/ico", sizes: "48x48", url: "/favicon.ico" },
+    icon: { type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+    apple: { type: "image/png", sizes: "180x180", url: "/apple-touch-icon.png" },
+    other: [
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", url: "/android-chrome-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "512x512", url: "/android-chrome-512x512.png" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
