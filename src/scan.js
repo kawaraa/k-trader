a9af5f04f6336a63ab9658a0c71426f67affe1f4
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-08-29 18:02:42 +0200

    Fitlering cryptos.

--- src/scan.js
+++ src/scan.js
@@ -9,6 +9,8 @@ const minProfitPercentage = process.argv[3] || 10;
 const minPercentageChange = process.argv[4] || 1.2;
 const fileNames = pairArg ? [`${pairArg}.json`] : readdirSync(pricesFolderPath);
 
+const cryptocurrenciesPricesChanges = [];
+
 for (const fileName of fileNames) {
   const pair = fileName.replace(".json", "");
   let prices = require(`${pricesFolderPath}${fileName}`);
@@ -18,26 +20,35 @@ for (const fileName of fileNames) {
   console.log(`Started prices analysis for "${pair}" with ${prices.length} prices`);
 
   let percentage = minPercentageChange;
+  let mostChanges = 0;
   while (percentage <= 25) {
     const result = countPriceChanges(prices, percentage);
+
     if (result.changes.at(-1) < 1) result.changes.pop();
     const pricesChanges = result.changes.filter((p) => p < -1).length / 2;
+    if (mostChanges < pricesChanges) mostChanges = pricesChanges;
+
     const estimateProfitPercentage = parseInt(percentage * pricesChanges);
 
-    if (estimateProfitPercentage >= minProfitPercentage) {
-      console.log(
-        "Pair:",
-        pair,
-        "Percentage:",
-        +percentage.toFixed(2),
-        "PricesChanges:",
-        pricesChanges,
-        "AvgPeriod:",
-        result.avgPeriod,
-        "EstimateProfitPercentage",
-        estimateProfitPercentage
-      );
-    }
+    // if (estimateProfitPercentage >= minProfitPercentage) {
+    //   console.log(
+    //     "Pair:",
+    //     pair,
+    //     "Percentage:",
+    //     +percentage.toFixed(2),
+    //     "PricesChanges:",
+    //     pricesChanges,
+    //     "AvgPeriod:",
+    //     result.avgPeriod,
+    //     "EstimateProfitPercentage",
+    //     estimateProfitPercentage
+    //   );
+    // }
     percentage += percentage < 2 ? 0.1 : 0.5;
   }
+  cryptocurrenciesPricesChanges.push({ pair, changes: mostChanges });
 }
+
+console.log(
+  JSON.stringify(cryptocurrenciesPricesChanges.sort((a, b) => b.changes - a.changes).map((item) => item.pair))
+);
