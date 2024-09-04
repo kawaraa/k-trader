a19c92e3fbb7419ab7e63f4b2295ba218e8e3269
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-09-04 11:46:17 +0200

    Testing currencies.

--- src/test-trading-script.js
+++ src/test-trading-script.js
@@ -2,25 +2,24 @@
 const { adjustPrice, countPriceChanges } = require("./trend-analysis.js");
 const TestExchangeProvider = require("./test-ex-provider.js");
 const DailyTrader = require("./daily-trader.js");
+const currencies = require("./currencies.json");
 const modes = ["high-drop", "near-low", "high-drop-partly-trade", "near-low-partly-trade"];
 
 const pair = process.argv[2]; // The pair of the two currency that will be used for trading E.g. ETHEUR
-const askBidSpreadPercentage = +process.argv[3] || 0.1; // A number from 0 to 100, the prices difference percent
-const capital = +process.argv[4] || 100; // Amount in EUR which is the total money that can be used for trading
-const investment = +process.argv[5] || 9;
-const minStrategyRange = +process.argv[6] || 0.25; // Is a Range of the strategy in days, min value from 0.25 day which equivalent to 6 hours
-const minPercentagePriceChange = +process.argv[7] || 1.25;
-const mode = process.argv[8] || modes[0];
+const capital = +process.argv[3] || 100; // Amount in EUR which is the total money that can be used for trading
+const investment = +process.argv[4] || 9;
+const minStrategyRange = +process.argv[5] || 0.25; // Is a Range of the strategy in days, min value from 0.25 day which equivalent to 6 hours
+const minPercentagePriceChange = +process.argv[6] || 1.25;
+const mode = process.argv[7] || modes[0];
 // const strategyInvestments = [9, 19, 32, 49, 99]; // strategySettings
 // investment is and investing Amount in EUR that will be used every time to by crypto
 // priceChange is a price Percentage Threshold, value from 0 to 100
-// Command example: node test-trading-script.js ETHEUR 0.9 0.25 100 60 > database/log/all.log 2>&1
+
+// Command example: node test-trading-script.js ETHEUR 100 99 0.25 1.1 near-low > database/log/all.log 2>&1
 
 if (!modes.includes(mode)) throw new Error("Invalid mode!");
 (async () => {
   console.log(`Started new trading with ${pair}.\n`);
-
-  let prices = require(`${process.cwd()}/database/test-prices/${pair}.json`);
 
   // const offset = parseInt((minStrategyRange * 24 * 60) / 5);
   // const result = countPriceChanges(prices, minPercentagePriceChange, offset);
@@ -29,25 +28,23 @@ if (!modes.includes(mode)) throw new Error("Invalid mode!");
   // const profit = parseInt(minPercentagePriceChange * pricesChanges);
   // console.log("Prices:", prices.length, "PricesChanges:", pricesChanges, "Profit:", profit);
 
-  if (!prices[0]?.tradePrice) prices = prices.map((p) => adjustPrice(p, askBidSpreadPercentage));
-
   try {
+    let prices = require(`${process.cwd()}/database/test-prices/${pair}.json`);
+    const askBidSpread = currencies[pair].askBidSpreadPercentage; //the prices difference percent between ask and bid prices
+    if (!prices[0]?.tradePrice) prices = prices.map((p) => adjustPrice(p, askBidSpread));
     let range = minStrategyRange;
     let priceChange = minPercentagePriceChange;
 
     const result = await testStrategy(pair, prices, capital, investment, range, priceChange);
+    const remain = parseInt(result.crypto);
+    const transactions = parseInt(result.transactions / 2);
+    // EUR: ${parseInt(result.balance)}
     console.log(
-      `Strategy: €${result.investment} | >${result.range}< | ${result.priceChange}%`,
-      "Balance: ",
-      result.balance,
-      "|",
-      result.crypto,
-      "=>",
-      +((result.balance - capital) / 2).toFixed(2),
-      `Transactions: ${result.transactions / 2}`
+      `€${capital} €${result.investment} >${result.range}< ${result.priceChange}% ${mode} =>`,
+      `€${parseInt((result.balance - capital) / 2)} Remain: ${remain} Transactions: ${transactions}`
     );
   } catch (error) {
-    console.log("Error with ", pair, "===>", error);
+    console.log("Error with ", pair, "=>", error);
   }
   console.log(`\n\n`);
 })();
