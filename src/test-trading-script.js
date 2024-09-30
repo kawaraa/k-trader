be2fd44c9942063d6a6ef89af67dc357a6b0050e
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-09-30 18:55:29 +0200

    Filtering currencies.

--- src/test-trading-script.js
+++ src/test-trading-script.js
@@ -5,7 +5,7 @@ const DailyTrader = require("./daily-trader.js");
 const currencies = require("./currencies.json");
 const modes = ["high-drop", "near-low", "high-drop-partly-trade", "near-low-partly-trade"];
 
-const pair = process.argv[2]; // The pair of the two currency that will be used for trading E.g. ETHEUR
+// const pair = process.argv[2]; // The pair of the two currency that will be used for trading E.g. ETHEUR
 const capital = +process.argv[3] || 100; // Amount in EUR which is the total money that can be used for trading
 const investment = +process.argv[4] || 9;
 const minStrategyRange = +process.argv[5] || 0.25; // Is a Range of the strategy in days, min value from 0.25 day which equivalent to 6 hours
@@ -18,35 +18,48 @@ const mode = process.argv[7] || modes[0];
 // Command example: node test-trading-script.js ETHEUR 100 99 0.25 1.1 near-low > database/log/all.log 2>&1
 
 if (!modes.includes(mode)) throw new Error("Invalid mode!");
+
+const pairs = Object.keys(currencies);
 (async () => {
-  console.log(`Started new trading with ${pair}.\n`);
+  for (const pair of pairs) {
+    console.log(`Started new trading with ${pair}.\n`);
 
-  // const offset = parseInt((minStrategyRange * 24 * 60) / 5);
-  // const result = countPriceChanges(prices, minPercentagePriceChange, offset);
-  // if (result.changes.at(-1) < 1) result.changes.pop();
-  // const pricesChanges = result.changes.filter((p) => p < -1).length / 2;
-  // const profit = parseInt(minPercentagePriceChange * pricesChanges);
-  // console.log("Prices:", prices.length, "PricesChanges:", pricesChanges, "Profit:", profit);
+    // const offset = parseInt((minStrategyRange * 24 * 60) / 5);
+    // const result = countPriceChanges(prices, minPercentagePriceChange, offset);
+    // if (result.changes.at(-1) < 1) result.changes.pop();
+    // const pricesChanges = result.changes.filter((p) => p < -1).length / 2;
+    // const profit = parseInt(minPercentagePriceChange * pricesChanges);
+    // console.log("Prices:", prices.length, "PricesChanges:", pricesChanges, "Profit:", profit);
 
-  try {
-    let prices = require(`${process.cwd()}/database/test-prices/${pair}.json`);
-    const askBidSpread = currencies[pair].askBidSpreadPercentage; //the prices difference percent between ask and bid prices
-    if (!prices[0]?.tradePrice) prices = prices.map((p) => adjustPrice(p, askBidSpread));
-    let range = minStrategyRange;
-    let priceChange = minPercentagePriceChange;
+    try {
+      const prices = require(`${process.cwd()}/database/prices/${pair}.json`);
+      // let prices = require(`${process.cwd()}/database/test-prices/${pair}.json`);
+      // const askBidSpread = currencies[pair].askBidSpreadPercentage; //the prices difference percent between ask and bid prices
+      // if (!prices[0]?.tradePrice) prices = prices.map((p) => adjustPrice(p, askBidSpread));
 
-    const result = await testStrategy(pair, prices, capital, investment, range, priceChange);
-    const remain = parseInt(result.crypto);
-    const transactions = parseInt(result.transactions / 2);
-    // EUR: ${parseInt(result.balance)}
-    console.log(
-      `€${capital} €${result.investment} >${result.range}< ${result.priceChange}% ${mode} =>`,
-      `€${parseInt((result.balance - capital) / 2)} Remain: ${remain} Transactions: ${transactions}`
-    );
-  } catch (error) {
-    console.log("Error with ", pair, "=>", error);
+      let maxBalance = 0;
+      for (let range = minStrategyRange; range <= 1; range += 0.25) {
+        for (let priceChange = minPercentagePriceChange; priceChange <= 7; priceChange += 0.5) {
+          const result = await testStrategy(pair, prices, capital, investment, range, priceChange);
+          const remain = parseInt(result.crypto);
+          // const transactions = parseInt(result.transactions / 2);
+          // const b = parseInt((result.balance - capital) / 2)
+          const transactions = parseInt(result.transactions);
+
+          if (maxBalance < result.balance + 3) {
+            maxBalance = result.balance;
+            console.log(
+              `€${capital} €${result.investment} >${result.range}< ${result.priceChange}% ${mode} =>`,
+              `€${parseInt(result.balance - capital)} Remain: ${remain} Transactions: ${transactions}`
+            );
+          }
+        }
+      }
+    } catch (error) {
+      console.log("Error with ", pair, "=>", error);
+    }
+    console.log(`\n\n`);
   }
-  console.log(`\n\n`);
 })();
 
 async function testStrategy(pair, prices, capital, investment, range, priceChange) {
