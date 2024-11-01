7ca4c3d61979137d5f0d344adff31e9b163b07dd
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-11-01 23:56:31 +0100

    Added the new strategy to the UI.

--- app/components/add-bot-form.js
+++ app/components/add-bot-form.js
@@ -50,6 +50,7 @@ export default function AddBotFrom({ bot, onSubmit }) {
       />
       <select name="mode" defaultValue={bot?.info?.mode} required className={inputCls}>
         <option value="">Mode</option>
+        <option value="on-increase">On increase</option>
         <option value="near-low">Near low</option>
         <option value="high-drop">High drop</option>
         <option value="near-low-partly-trade">Near low and partly trade</option>
