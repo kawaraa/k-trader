76539f65c0a21a8e62c209cc37095243cff48428
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-05-07 10:40:46 +0200

    Improving.

--- src/trader/advance-trader.js
+++ src/trader/advance-trader.js
@@ -33,7 +33,7 @@ class AdvanceTrader extends Trader {
         this.dispatch("LOG", `${testLog} Placed BUY at: ${currentPrice.askPrice}`);
       }
       //
-    } else if (decision === "SELLxxx") {
+    } else if (decision === "SELL") {
       this.dispatch("LOG", `${testLog} [-] Breakdown detected - Position: ${positionLog}.`);
       if (position) {
         const res = await this.sell(position, balance, currentPrice.bidPrice);
