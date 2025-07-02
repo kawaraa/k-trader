234acc74a6094bac99ac34107d882fda8e80ce8d
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-07-02 15:43:09 +0200

    Fixing notifications v-1.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // self.importScripts('foo.js', 'bar.js');
-const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984it0y45plmlbaplalk";
+const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984it0y45plmlbaplal";
 const staticFileCachePaths = ["/"];
 
 self.addEventListener("install", (evt) => {
