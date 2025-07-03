72e7058b1d1b288623eb76a4d9acabced3382ffa
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-07-03 13:39:53 +0200

    Updated sw.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // self.importScripts('foo.js', 'bar.js');
-const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984it0y45plmlbapl";
+const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984it0y45plmlbap";
 const staticFileCachePaths = ["/"];
 
 self.addEventListener("install", (evt) => {
