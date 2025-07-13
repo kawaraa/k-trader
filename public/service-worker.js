a396adefc240c2aacfe935e6474db5e3f02a5b01
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-07-13 22:18:38 +0200

    Updated SW.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // self.importScripts('foo.js', 'bar.js');
-const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984it";
+const staticFileCacheName = "static-files-v-0xbcy2ye7y3ubfhwvdtw1tye984i";
 const staticFileCachePaths = [
   "/offline.html",
   "/manifest.json",
